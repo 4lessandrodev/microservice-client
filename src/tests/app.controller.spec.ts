60287375a26bc61d7fbcc6c4f20b3e253f327ce1
4lessandrodev <alessandroadm@live.com>
2022-09-03 14:06:15 -0300

    chore(env): set env

--- src/tests/app.controller.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AppController } from '../app.controller';
-
-describe('app.controller', () => {
-  it('should be defined', () => {
-    const controller = AppController;
-    expect(controller).toBeDefined();
-  });
-});
