ab2c191459342373d526185c309a5faf1b19f4fc
4lessandrodev <alessandroadm@live.com>
2022-09-03 13:59:50 -0300

    chore(env): set env

--- src/shared/config/env.ts
+++ src/shared/config/env.ts
@@ -3,8 +3,8 @@ import * as env from 'env-var';
 
 export const NODE_ENV = env
   .get('NODE_ENV')
-  .required()
   .default('dev')
+  .required()
   .asEnum(['dev', 'qas', 'uat', 'production']);
 
 export const URL_CONNECTION = env.get('URL_CONNECTION').required().asString();
@@ -13,8 +13,8 @@ export const QUEUE_NAME = env.get('QUEUE_NAME').required().asString();
 
 export const TRANSPORTER = env
   .get('TRANSPORTER')
-  .required()
   .default('RMQ')
+  .required()
   .asEnum(['TCP', 'REDIS', 'NATS', 'MQTT', 'GRPC', 'RMQ', 'KAFKA']);
 
 export const PORT = env.get('PORT').default(3000).required().asPortNumber();
